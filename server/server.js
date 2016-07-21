ed7f7f3c3665647ca7c0e6fd0011dc742f30d0a8
Frank Lee <flee531@gmail.com>
2016-07-21 12:07:56 -0700

    Handle GET request for '/'

--- server/server.js
+++ server/server.js
@@ -13,7 +13,11 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client'));
-app.use(express.static(__dirname + '/'));
+// app.use(express.static(__dirname + '/'));
+
+app.get('/', function(request, response) {
+  response.render('client/index.html');
+});
 
 // Database connection
 db.connection.connect(error => {
