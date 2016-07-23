9067b02297a032a59b22462d193b6d31862c2fa5
Jae Shin <jae5464@gmail.com>
2016-07-23 16:26:50 -0700

    Add models for mongoDB

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ var db = require('./db/db');
 
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var app = express();
 app.set('port', (process.env.PORT || 8000));
@@ -14,6 +15,11 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client'));
+app.use(session({
+  secret: 'good eats is a secret',
+  resave: false,
+  saveUninitialized: true
+}));
 
 // Routing
 require('./config/routes')(app, express);
