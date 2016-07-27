e63fd9908c5a8cd4c37710d6fcc9dd7dee55aa35
Matthew Aftalion <maftalion@Matthews-MacBook-Pro.local>
2016-07-26 20:42:20 -0700

    Adding Socket functionality

--- server/server.js
+++ server/server.js
@@ -1,23 +1,7 @@
 var express = require('express');
-<<<<<<< c6c3ce9dabeed06f899c2abe3eb08990ad72f54f
 var http = require('http');
-<<<<<<< 4f21d606c063e39518a93b29191c55d148538ab5
-<<<<<<< 8bdb5bc66fa30672418937138e7a25a26679e774
-// MongoDB connection
-=======
-var io = require('socket.io')(app);
 // MongoDB connection
 var http = require('http');
-var http = require('http');
->>>>>>> Adding Socket functionality
-=======
-<<<<<<< c47498f3b030d3aa758756057caa6709b3287c4a
-var io = require('socket.io')(app);
-// MongoDB connection
-var http = require('http');
-var io = require('socket.io')(app);
-// MongoDB connection
->>>>>>> Initial set up of Socket.io
 var db = require('./db/db');
 var app = express();
 var server = http.createServer(app);
@@ -73,12 +57,12 @@ io.on('connection', function (socket) {
     socket.broadcast.emit('update', data);
   });
 
+  socket.on('playerData', function (data) {
+    io.emit('playerNote', data);
+  });
   socket.on('disconnect', function () {
     io.emit('user disconnected');
     delete clients[socket.id];
-  });
-  socket.on('playerData', function (data) {
-    io.emit('playerNote', data);
   });
 });
 // app.listen(app.get('port'), function() {
