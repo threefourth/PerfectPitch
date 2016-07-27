678d591014a449d0608dea21e4d321ce7fe5e271
Matthew Aftalion <maftalion@Matthews-MacBook-Pro.local>
2016-07-27 10:19:14 -0700

    Setting up Sockets

--- client/socketEvents.js
+++ client/socketEvents.js
@@ -1,11 +1,11 @@
-require('socket.io-client');
-$(document).on('click', function(event){
-    console.log('Something has been clicked!' + event.target);
-    socket.emit('myClick', {id: event.target});
-});
-
-var socket = io.connect('http://localhost');
-
-socket.on('myClick', function (data) {
-    $(data.id).trigger('click');
-});
+// 
+// $(document).on('click', function(event){
+//     console.log('Something has been clicked!' + event.target);
+//     socket.emit('myClick', {id: event.target});
+// });
+//
+// var socket = io.connect('http://localhost');
+//
+// socket.on('myClick', function (data) {
+//     $(data.id).trigger('click');
+// });
