8bdb5bc66fa30672418937138e7a25a26679e774
Matt <aftalion.matt@gmail.com>
2016-07-28 11:59:06 -0700

    rebasing

--- client/components/PitchVisualizer.jsx
+++ client/components/PitchVisualizer.jsx
@@ -355,7 +355,7 @@ export default class PitchVisualizer extends React.Component {
         .remove();
     };
 
-    var that = this;
+    // var that = this;
 
     // var setScore = function(score, perfectCount) {
     //   this.setState({
