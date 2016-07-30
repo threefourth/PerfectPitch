b8f63874071048518a6974c84a6447adcfeb2532
Hongchao Yu <yuhongchao@gmail.com>
2016-07-30 14:24:44 -0700

    Added opponent's score

--- client/components/Score.jsx
+++ client/components/Score.jsx
@@ -4,7 +4,8 @@ export default class Score extends React.Component {
 	constructor(props) {
     super(props);
     this.state = {
-      percentage: 0
+      percentage: 0,
+      opponentPercentage: 0
     }
   }
 
@@ -17,14 +18,15 @@ export default class Score extends React.Component {
   updateScoreBoard () {
   	var that = this;
   	this.props.updateScore(function(result){
-  		that.setState({percentage: result});
+  		that.setState({percentage: result.percentage, opponentPercentage: result.opponentPercentage});
   	});
   }
 
 	render() {
 		return(
       <div className="col l4 s4 scoreboard offset-l3">
-        <span>Accu. Acurracy : {this.state.percentage} %</span>
+        <span>Your Score : {this.state.percentage} %</span><br />
+        <span>Your Friend's Score : {this.state.opponentPercentage} %</span>
       </div>
     );
   }
